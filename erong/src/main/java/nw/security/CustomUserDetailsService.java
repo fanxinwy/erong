635fff298776baff6e1870dc1f57c02643880e3c
wukairobin <wukairobin@gmail.com>
2015-06-06 11:12:07 +0800

    security

---
+++ erong/src/main/java/nw/security/CustomUserDetailsService.java
@@ -0,0 +1,17 @@
+package nw.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+		
+		UserDetails ud = null;
+		
+		return ud;
+	}
+
+}
